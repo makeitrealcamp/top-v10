cb62c8d68a90d7b815ce0d81c3225e74a7f80a13
Simon Hoyos <shmesa23@gmail.com>
2021-08-25 21:01:28 -0500

    React Components

--- react/first-steps/src/App.js
+++ react/first-steps/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,8 +9,94 @@ import './App.css';
 // class -> className
 // for -> htmlFor
 
-function Greeting({ name, age }) {
-  return <p>Hola {name} {age}</p>
+// true && true // true
+// true && false // false
+// false && true // false
+// false && false // false
+
+// true && 'hola mundo' // 'hola mundo'
+// 0 && 'hola mundo' // 0
+
+// true || true // true
+// true || false // true
+// false || true // true
+// false || false // false
+
+// const port = process.env.PORT || 3000
+
+function Greeting({ name, age, working }) {
+  return (
+    <div>
+      <p>Hola {name} {age}</p>
+      {working && <p>Working</p>}
+      {working ? (
+        <p>Working</p>
+      ) : (
+        false
+      )}
+    </div>
+  )
+}
+
+function Button({ children }) {
+  return <button>{children}</button>
+}
+
+class TrafficLight extends React.Component {
+  // constructor(props) {
+  //   super(props)
+
+  //   // stop, prepare, go
+  //   this.state = {
+  //     signal: 'prepare'
+  //   }
+
+  //   this.handleStop = this.handleStop.bind(this)
+  //   this.handlePrepare = this.handlePrepare.bind(this)
+  //   this.handleGo = this.handleGo.bind(this)
+  // }
+  state = {
+    signal: 'go'
+  }
+
+  handleStop = () => {
+    this.setState({ signal: 'stop' })
+    // this.state.signal = 'stop'
+  }
+
+  handlePrepare = () => {
+    this.setState({ signal: 'prepare' })
+  }
+
+  handleGo = () => {
+    this.setState({ signal: 'go' })
+  }
+
+  render() {
+    const light = {
+      stop: 'red',
+      prepare: 'yellow',
+      go: 'green',
+    }
+
+    // let light;
+    // if(this.state.signal === 'stop') {
+    //   light = 'red'
+    // } else if (this.state.signal === 'prepare') {
+    //   light = 'yellow'
+    // } else {
+    //   light = 'green'
+    // }
+
+    return (
+      <div>
+        <p>{light[this.state.signal]}</p>
+        <button type="button" onClick={this.handleStop}>Stop</button>
+        <button type="button" onClick={this.handlePrepare}>Prepare</button>
+        <button type="button" onClick={this.handleGo}>Go</button>
+      </div>
+    )
+  }
 }
 
 function App() {
@@ -20,10 +107,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload. Hola mundo
         </p>
-        <Greeting name="maria" age={19} />
+        <TrafficLight />
+        <TrafficLight />
+        <TrafficLight />
+        <TrafficLight />
+        <Greeting name="maria" age={19} working />
         <Greeting name="juan" />
         <Greeting name="ana" />
         <Greeting name="martin" />
+        <Button>Hola mundo</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
